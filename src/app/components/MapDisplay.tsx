73f4aa96ce592325ec86286ca675d5150d840ab1
nick-brown2014 <nick.brown2014@gmail.com>
2025-05-22 10:54:37 -0600

    one more error

--- src/app/components/MapDisplay.tsx
+++ src/app/components/MapDisplay.tsx
@@ -1,12 +1,6 @@
 'use client'
 
-import useMapDisplay from "../hooks/useMapDisplay"
-
-
-
-
 const MapDisplay = () => {
-  const { } = useMapDisplay()
 
   return (
     <div className='w-full flex'>
