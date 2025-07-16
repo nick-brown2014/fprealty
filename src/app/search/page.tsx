c92b8ca0a8d35e270b6e7190d23bc330b323571e
nick-brown2014 <nick.brown2014@gmail.com>
2025-07-15 20:42:11 -0600

    search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,20 +1,37 @@
 'use client'
 
+import Footer from "../components/Footer"
 import Nav from "../components/Nav"
 import { useEffect } from "react"
 
 const Home = () => {
 
   useEffect(() => {
-    const script = document.createElement('script')
-    script.src = '//jaycee.idxbroker.com/idx/widgets/77302'
-    script.id = 'idxwidgetsrc-77302'
-    script.type = 'text/javascript'
-    script.async = true
-    document.getElementById('find-map-widget')?.appendChild(script)
+    const mapScript = document.createElement('script')
+    mapScript.src = '//nocorealtor.idxbroker.com/idx/widgets/108423'
+    mapScript.id = 'idxwidgetsrc-108423'
+    mapScript.type = 'text/javascript'
+    mapScript.async = true
+    document.getElementById('find-map-widget')?.appendChild(mapScript)
+
+    const showcaseScript = document.createElement('script')
+    showcaseScript.src = '//nocorealtor.idxbroker.com/idx/widgets/104535'
+    showcaseScript.id = 'idxwidgetsrc-104535'
+    showcaseScript.type = 'text/javascript'
+    showcaseScript.async = true
+    document.getElementById('showcase-listing-widget')?.appendChild(showcaseScript)
+
+    const searchScript = document.createElement('script')
+    searchScript.src = '//nocorealtor.idxbroker.com/idx/widgets/107914'
+    searchScript.id = 'idxwidgetsrc-107914'
+    searchScript.type = 'text/javascript'
+    searchScript.async = true
+    document.getElementById('search-widget')?.appendChild(searchScript)
 
     return () => {
-      document.getElementById('wid77302')?.remove()
+      document.getElementById('wid104535')?.remove()
+      document.getElementById('wid108423')?.remove()
+      document.getElementById('wid107914')?.remove()
     }
   }, [])
 
@@ -22,7 +39,12 @@ const Home = () => {
     <div className='w-full h-full flex-col'>
       <Nav />
       <div className='px-6 py-10 flex flex-col'>
+          <div id='search-widget' />
           <div id='find-map-widget' />
+          <div className='mt-8' />
+          <div id='showcase-listing-widget' />
+          <div className='mt-8' />
+          <Footer />
       </div>
     </div>
   )
