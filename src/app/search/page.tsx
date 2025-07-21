f6aca089db213580c3076ebc48362ddfcd437c0c
nick-brown2014 <nick.brown2014@gmail.com>
2025-07-21 12:26:02 -0600

    page as iframe

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,37 +2,38 @@
 
 import Footer from "../components/Footer"
 import Nav from "../components/Nav"
-import { useEffect } from "react"
+// import { useEffect } from "react"
 
 const Home = () => {
 
-  useEffect(() => {
-    const mapScript = document.createElement('script')
-    mapScript.src = '//nocorealtor.idxbroker.com/idx/widgets/108423'
-    mapScript.id = 'idxwidgetsrc-108423'
-    mapScript.type = 'text/javascript'
-    mapScript.async = true
-    document.getElementById('find-map-widget')?.appendChild(mapScript)
+  // useEffect(() => {
+  //   const mapScript = document.createElement('script')
+  //   mapScript.src = '//nocorealtor.idxbroker.com/idx/widgets/108423'
+  //   mapScript.id = 'idxwidgetsrc-108423'
+  //   mapScript.type = 'text/javascript'
+  //   mapScript.async = true
+  //   document.getElementById('find-map-widget')?.appendChild(mapScript)
 
-    const searchScript = document.createElement('script')
-    searchScript.src = '//nocorealtor.idxbroker.com/idx/widgets/107914'
-    searchScript.id = 'idxwidgetsrc-107914'
-    searchScript.type = 'text/javascript'
-    searchScript.async = true
-    document.getElementById('search-widget')?.appendChild(searchScript)
+  //   const searchScript = document.createElement('script')
+  //   searchScript.src = '//nocorealtor.idxbroker.com/idx/widgets/107914'
+  //   searchScript.id = 'idxwidgetsrc-107914'
+  //   searchScript.type = 'text/javascript'
+  //   searchScript.async = true
+  //   document.getElementById('search-widget')?.appendChild(searchScript)
 
-    return () => {
-      document.getElementById('wid108423')?.remove()
-      document.getElementById('wid107914')?.remove()
-    }
-  }, [])
+  //   return () => {
+  //     document.getElementById('wid108423')?.remove()
+  //     document.getElementById('wid107914')?.remove()
+  //   }
+  // }, [])
 
   return (
     <div className='w-full h-full flex-col'>
       <Nav />
-      <div className='px-6 pb-10 pt-4 flex flex-col'>
-          <div id='search-widget' />
-          <div id='find-map-widget' />
+      <div className='pb-10 pt-4 flex flex-col w-[100vw] justify-center'>
+          {/* <div id='search-widget' />
+          <div id='find-map-widget' /> */}
+          <iframe src='https://nocorealtor.idxbroker.com/idx/map/mapsearch' width='95%' height={1000} />
           <div className='mt-8' />
           <Footer />
       </div>
