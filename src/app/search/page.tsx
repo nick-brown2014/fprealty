a9f0ac1fa5ae8dc5779b2a84a170c47d78ff80b9
nick-brown2014 <nick.brown2014@gmail.com>
2025-07-21 11:09:00 -0600

    removed showcase

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -14,13 +14,6 @@ const Home = () => {
     mapScript.async = true
     document.getElementById('find-map-widget')?.appendChild(mapScript)
 
-    const showcaseScript = document.createElement('script')
-    showcaseScript.src = '//nocorealtor.idxbroker.com/idx/widgets/104535'
-    showcaseScript.id = 'idxwidgetsrc-104535'
-    showcaseScript.type = 'text/javascript'
-    showcaseScript.async = true
-    document.getElementById('showcase-listing-widget')?.appendChild(showcaseScript)
-
     const searchScript = document.createElement('script')
     searchScript.src = '//nocorealtor.idxbroker.com/idx/widgets/107914'
     searchScript.id = 'idxwidgetsrc-107914'
@@ -29,7 +22,6 @@ const Home = () => {
     document.getElementById('search-widget')?.appendChild(searchScript)
 
     return () => {
-      document.getElementById('wid104535')?.remove()
       document.getElementById('wid108423')?.remove()
       document.getElementById('wid107914')?.remove()
     }
@@ -38,11 +30,9 @@ const Home = () => {
   return (
     <div className='w-full h-full flex-col'>
       <Nav />
-      <div className='px-6 py-10 flex flex-col'>
+      <div className='px-6 pb-10 pt-4 flex flex-col'>
           <div id='search-widget' />
           <div id='find-map-widget' />
-          <div className='mt-8' />
-          <div id='showcase-listing-widget' />
           <div className='mt-8' />
           <Footer />
       </div>
