2567f5d2e9ed857a4380b8121192d49d4c98b766
nick-brown2014 <nick.brown2014@gmail.com>
2025-07-23 14:41:21 -0600

    context cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { CookiesProvider } from 'next-client-cookies/server';
-import { AuthProvider } from "./contexts/AuthContext";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,9 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <CookiesProvider>
-          <AuthProvider>
             {children}
-          </AuthProvider>
         </CookiesProvider>
       </body>
     </html>
