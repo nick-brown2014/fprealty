80d8b79a0e776636c23970a22702abb618cd25ae
nick-brown2014 <nick.brown2014@gmail.com>
2026-01-11 10:48:02 -0700

    updated homebot link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -121,7 +121,7 @@ export default function NewHome() {
               />
             </div>
             <a
-              href='https://www.homebot.ai'
+              href='https://hmbt.co/iAnytx'
               target='_blank'
               rel='noopener noreferrer'
               className='cursor-pointer bg-white hover:bg-gray-100 text-[#f51e84] px-6 py-2 rounded-full text-sm font-semibold tracking-wide transition-all duration-300 shadow-lg hover:shadow-xl transform hover:scale-105'
